e96b614127426bfdee26fc1d6df70228adc27f3a
ciaran-regan-ie <ciaranregan123@gmail.com>
2024-01-04 19:11:30 +0900

    wip

--- empirical/main.py
+++ empirical/main.py
@@ -15,10 +15,10 @@ class Params:
 
 if __name__ == "__main__":
     targets = {
-        "aps": Params(rho=4, nu=10, s="asw", zeta=0.5, eta=0.5, steps=1),
-        "tmn": Params(rho=2, nu=1, s="asw", zeta=0.5, eta=0.5, steps=1),
-        "ida": Params(rho=3, nu=4, s="asw", zeta=0.5, eta=0.5, steps=1),
-        "eight": Params(rho=1, nu=2, s="asw", zeta=0.5, eta=0.5, steps=1),
+        "aps": Params(rho=4, nu=10, s="asw", zeta=0.5, eta=0.5, steps=1000000),
+        "tmn": Params(rho=2, nu=1, s="asw", zeta=0.5, eta=0.5, steps=1000000),
+        "ida": Params(rho=3, nu=4, s="asw", zeta=0.5, eta=0.5, steps=1000000),
+        "eight": Params(rho=1, nu=2, s="asw", zeta=0.5, eta=0.5, steps=1000000),
     }
 
     innovation_types = {
